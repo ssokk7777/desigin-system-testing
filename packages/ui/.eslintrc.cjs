eef08fe28b0f44ad30daadb3ee71d964f85719f0
sohyunkk <so--hyun.kim@kt.com>
2024-12-17 14:38:35 +0900

    user-flow e2e cypress github action 배포 test

--- packages/ui/.eslintrc.cjs
+++ packages/ui/.eslintrc.cjs
@@ -1,11 +1,12 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ['@kt-cloud-front/eslint-config', 'plugin:storybook/recommended'],
+  extends: ['@kt-cloud-front/eslint-config', 'plugin:storybook/recommended', 'plugin:cypress/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: true,
     tsconfigRootDir: __dirname,
     createDefaultProgram: true,
   },
+  plugins: ['cypress'],
   ignorePatterns: ['.eslintrc.cjs', 'vite.config.ts', 'dist'],
 }
