bdeab5782d0d51056ff18399cd3139f0b5eee88c
sohyunkk <so--hyun.kim@kt.com>
2025-01-07 09:40:59 +0900

    switch 컴포넌트 수정

--- packages/ui/src/components/switch/Switch.stories.tsx
+++ packages/ui/src/components/switch/Switch.stories.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react'
 import type { StoryObj } from '@storybook/react'
 import { Switch } from './Switch'
 import { SwitchProps } from "./Switch"
 import StoryTemplate from '@kt-cloud-front/ui/common/StoryTemplate'
+import {userEvent, within} from "@storybook/test";
+import { expect } from '@storybook/jest';
 
 const colorOptions = ['primary', 'secondary', 'success', 'error', 'warning'] as const
 const sizeOptions = ['small', 'medium', 'large'] as const
@@ -23,7 +25,9 @@ const meta: IMeta = {
   parameters: {
     layout: 'centered',
   },
-  tags: ['autodocs', '!dev'],
+  tags: ['autodocs',
+    // '!dev'
+  ],
   argTypes: {
     color: {
       description: 'switch의 색상을 설정합니다.',
@@ -42,8 +46,40 @@ type Story = StoryObj<SwitchProps>
 
 export const Default: Story = {
   args: {
-    label: 'Switch',
-    name: 'Switch'
+
+  },
+  render: (args) => {
+    const [checked, setChecked] = useState(false);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setChecked(e.target.checked);
+    };
+
+    return (
+      <Switch
+        {...args}
+        checked={checked}
+        onChange={handleChange}
+        label={checked ? 'Checked' : 'Unchecked'}
+      />
+    );
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const switchInput = canvas.getByRole('switch', { name: 'Unchecked' });
+
+    // 클릭 전 checked=false 인지 확인
+    await expect(switchInput).not.toBeChecked();
+
+    // switch 클릭
+    await userEvent.click(switchInput);
+
+    // 클릭 후 checked=true 변경 확인
+    await expect(switchInput).toBeChecked();
+
+    // 클릭 후 레이블이 "Checked"로 변경되었는지 확인
+    const label = canvas.getByText('Checked'); // DOM에서 텍스트가 'Checked'인 요소를 찾음
+    await expect(label).toBeInTheDocument(); // 찾은 요소(label)가 DOM에 존재하는지 확인
   },
 }
 
